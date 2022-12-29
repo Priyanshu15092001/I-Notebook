b71d38f402dc747226fac630568a4c94d3e91f4f
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2022-12-29 18:15:06 +0530

    Create User Commit
    Added Validation, JWT Authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,33 +2,65 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET='abcdef';
 
-//Create a user using: POST "/api/auth/" Doesnt require auth
+//Create a user using: POST "/api/auth/createUser" Doesnt require auth
 router.post(
-  "/",
+  "/createUser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password must be min 5 length.").isLength({ min: 5 }),
   ],
-  (req, res) => {
-    console.log("abc")
+  async (req, res) => {
+    //if there are errors return bad request and the errors
+    console.log("abc");
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    //check whether the user with this email exist already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exist" });
+      }
+      //Create a new user
+const salt=await bcrypt.genSalt(10);
+      secPass= await bcrypt.hash(req.body.password,salt);
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,}).then(user=>res.json(user)).catch(err=>{console.log(err)
-        res.json({error:'Already registered mail'})});
-    // const user=User(req.body);
-    // user.save();
-    //res.send();
-    })
-  // const data= user.save()
-     // const data ={}
+        password: secPass,
+      });
+      // .then(user=>res.json(user)).catch(err=>{console.log(err)
+      // res.json({error:'Already registered mail'})});
+const data={
+  user:{
+    id:user.id
+  }
+}
+      const authToken=jwt.sign(data,JWT_SECRET);
+     // console.log(authToken);
+     // res.json({ user });
+res.json({authToken});
 
-  //  res.json({data});
-  
+
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
+  }
+);
+
+
+
+
+
+
 module.exports = router;
