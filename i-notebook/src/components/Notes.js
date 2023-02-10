c943aa31c136956bb55d7fe780b4f9fda0d64327
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2023-02-10 22:41:19 +0530

    Add Note UI commit
    Add note UI functionality

--- i-notebook/src/components/Notes.js
+++ i-notebook/src/components/Notes.js
@@ -2,17 +2,21 @@ import React from 'react'
 import noteContext from '../context/notes/noteContext';
 import { useContext } from 'react';
 import NoteItem from './NoteItem';
+import AddNote from './AddNote';
 export const Notes = () => {
     const context = useContext(noteContext);
-    const {notes,setnotes}=context;
+    const {notes,addNotes}=context;
     
     
   return (
+    <>
+    <AddNote/>
     <div className="row my-3">
     <h1>Your Notes</h1>
     {notes.map((notes)=>{
         return <NoteItem notes ={notes} key={notes._id} />
     })}
   </div>
+  </>
   )
 }
--- i-notebook/src/context/notes/NoteState.js
+++ i-notebook/src/context/notes/NoteState.js
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
-const NoteState=(props)=>{
-    const notesInitial=[
-        {
-          "_id": "63db445065092a0464fe53d4",
-          "user": "63ac35ff4e067b9cd14e230e",
-          "title": "My title",
-          "description": "please wake up early",
-          "tag": "personal",
-          "date": "2023-02-02T05:04:16.683Z",
-          "__v": 0
-        },
-        {
-          "_id": "63e086f265092a0464fe53d7",
-          "user": "63ac35ff4e067b9cd14e230e",
-          "title": "My title11",
-          "description": "please wake up early",
-          "tag": "personal",
-          "date": "2023-02-06T04:49:54.436Z",
-          "__v": 0
-        }
-      ]
-      const [notes, setnotes] = useState(notesInitial)
-return (
-    <NoteContext.Provider value={{notes,setnotes}}>
-        {props.children}
+const NoteState = (props) => {
+  const notesInitial = [
+    {
+      _id: "63db445065092a0464fe53d4",
+      user: "63ac35ff4e067b9cd14e230e",
+      title: "My title",
+      description: "please wake up early",
+      tag: "personal",
+      date: "2023-02-02T05:04:16.683Z",
+      __v: 0,
+    },
+    {
+      _id: "63e086f265092a0464fe53d7",
+      user: "63ac35ff4e067b9cd14e230e",
+      title: "My title11",
+      description: "please wake up early",
+      tag: "personal",
+      date: "2023-02-06T04:49:54.436Z",
+      __v: 0,
+    },
+  ];
+  const [notes, setnotes] = useState(notesInitial);
+
+  //Add a note
+  const addNote = (title,description,tag) => {
+  const  note={
+    "_id": "63e086f265092a0464fe53d74",
+    "user": "63ac35ff4e067b9cd14e230e",
+    "title": title,
+    "description": description,
+    "tag": tag,
+    "date": "2023-02-06T04:49:54.436Z",
+    "__v": 0,
+    }
+    
+    setnotes(notes.concat(note))
+  };
+
+  //Delete a note
+  const deleteNote = () => {
+
+  };
+
+  //Edit a note
+  const editNote = () => {};
+
+  return (
+    <NoteContext.Provider value={{ notes, addNote,deleteNote,editNote }}>
+      {props.children}
     </NoteContext.Provider>
-)
-}
+  );
+};
 export default NoteState;
